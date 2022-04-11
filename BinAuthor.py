44c31a1b230e712ec5f95d6af0a730387fdc1380
Benthin, Marius <Marius.Benthin@gdata.de>
2022-04-11 15:26:54 +0200

    Adjusted BinAuthor.py plugin loader

--- BinAuthor.py
+++ BinAuthor.py
@@ -1,58 +1,44 @@
-import sys
-from idautils import *
-from idaapi import *
-import os
-from os import listdir
-from os.path import isfile
-import copy
-from pymongo import MongoClient
-from datetime import datetime
-import hashlib
-import idc
-from sark.qt import QtGui, QtCore
-import sark
-import sark.ui
-import sark.qt
-from idaapi import plugin_t
-from pprint import pprint
-from idaapi import PluginForm
-
-import pluginConfigurations
-import BinAuthorPlugin.Algorithms.FunctionFliterAndColorizer as FunctionFilter
-import BinAuthorPlugin.PluginMenuManager.BinAuthorManager as BinAuthorManager
-
-class BinAuthor_plugin_t(plugin_t):
-    flags = idaapi.PLUGIN_PROC
-    comment = "Author Identification using Novel Techniques"
-    help = "Help if a matter of trust."
-    wanted_name = "BinAuthor"
-    wanted_hotkey = ""
-        
-    def init(self):
-        try:
-            self.BinAuthorFunctionFilter = FunctionFilter.FunctionFilter()
-        except:
-            idaapi.msg("Failed to initialize Function Filter.\n")
-            idaapi.msg("Failed to initialize BinAuthor.\n")
-            del self.BinAuthorFeatureExtractor
-            idaapi.msg("Errors and fun!\n")
-            return idaapi.PLUGIN_SKIP
-            
-        try:
-            self.BinAuthor_manager = BinAuthorManager.BinAuthorManager()
-            self.BinAuthor_manager.buildMenu(self.BinAuthorFunctionFilter)
-            return idaapi.PLUGIN_KEEP
-        except:
-            idaapi.msg("Failed to initialize BinAuthor.\n")
-            del self.BinAuthor_manager
-            idaapi.msg("Errors and fun!\n")
-            return idaapi.PLUGIN_SKIP
-        
-        
-    def term(self):
-        self.BinAuthor_manager.del_menu_items()
-
-    def run(self, arg):
-        pass
 def PLUGIN_ENTRY():
+
+    from ida_kernwin import msg, warning
+    from ida_idaapi import plugin_t, PLUGIN_PROC, PLUGIN_SKIP, PLUGIN_KEEP
+
+    from BinAuthorPlugin.PluginMenuManager import BinAuthorManager as BinAuthorManager
+    from BinAuthorPlugin.Algorithms import FunctionFliterAndColorizer as FunctionFilter
+
+    class BinAuthor_plugin_t(plugin_t):
+        flags = PLUGIN_PROC
+        comment = "Author Identification using Novel Techniques"
+        help = "Help if a matter of trust."
+        wanted_name = "BinAuthor"
+        wanted_hotkey = ""
+
+        def init(self):
+            try:
+                self.BinAuthorFunctionFilter = FunctionFilter.FunctionFilter()
+            except Exception as e:
+                msg(e)
+                warning("Failed to initialize Function Filter.\n")
+                warning("Failed to initialize BinAuthor.\n")
+                del self.BinAuthorFeatureExtractor
+                warning("Errors and fun!\n")
+                return PLUGIN_SKIP
+
+            try:
+                self.BinAuthor_manager = BinAuthorManager.BinAuthorManager()
+                self.BinAuthor_manager.buildMenu(self.BinAuthorFunctionFilter)
+                return PLUGIN_KEEP
+            except Exception as e:
+                msg(e)
+                warning("Failed to initialize BinAuthor.\n")
+                del self.BinAuthor_manager
+                warning("Errors and fun!\n")
+                return PLUGIN_SKIP
+
+        def term(self):
+            self.BinAuthor_manager.del_menu_items()
+
+        def run(self, arg):
+            pass
+
     return BinAuthor_plugin_t()
