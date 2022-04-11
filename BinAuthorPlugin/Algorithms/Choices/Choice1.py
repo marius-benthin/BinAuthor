85fe25d8e4861144c1bea6f325db22db29b7e6e6
Benthin, Marius <Marius.Benthin@gdata.de>
2022-04-11 15:11:39 +0200

    Replaced math.log with log

--- BinAuthorPlugin/Algorithms/Choices/Choice1.py
+++ BinAuthorPlugin/Algorithms/Choices/Choice1.py
@@ -71,56 +71,56 @@ class Choice1():
         output["regs used"] = instructionCounts["reg_used"]
 
         if float(totalInstructions) != 0 :
-            output["ln(num push/length)"] = math.log(instructionCounts["push"]/float(totalInstructions))
-            output["ln(num call/length)"] = math.log(instructionCounts["call"]/float(totalInstructions))
+            output["ln(num push/length)"] = log(instructionCounts["push"]/float(totalInstructions))
+            output["ln(num call/length)"] = log(instructionCounts["call"]/float(totalInstructions))
         else:
             output["ln(num push/length)"] = "infinity"
             output["ln(num call/length)"] = "infinity"
         if float(totalInstructions) != 0:
             if (instructionCounts["indirect_call"]/float(totalInstructions)) != 0 and (instructionCounts["reg_used"]/float(totalInstructions)) != 0:
-                output["ln(num Indirect call/length)"] = math.log(instructionCounts["indirect_call"]/float(totalInstructions))
-                output["ln(num reg used/length)"] = math.log(instructionCounts["reg_used"]/float(totalInstructions))
+                output["ln(num Indirect call/length)"] = log(instructionCounts["indirect_call"]/float(totalInstructions))
+                output["ln(num reg used/length)"] = log(instructionCounts["reg_used"]/float(totalInstructions))
             elif (instructionCounts["indirect_call"]/float(totalInstructions)) == 0:
                 output["ln(num Indirect call/length)"] = "infinity"
-                output["ln(num reg used/length)"] = math.log(instructionCounts["reg_used"]/float(totalInstructions))
+                output["ln(num reg used/length)"] = log(instructionCounts["reg_used"]/float(totalInstructions))
             elif (instructionCounts["reg_used"]/float(totalInstructions)) == 0:
-                output["ln(num Indirect call/length)"] = math.log(instructionCounts["indirect_call"]/float(totalInstructions))
+                output["ln(num Indirect call/length)"] = log(instructionCounts["indirect_call"]/float(totalInstructions))
                 output["ln(num reg used/length)"] = "infinity"
         else:
             output["ln(num Indirect call/length)"] = "infinity"
             output["ln(num reg used/length)"] = "infinity"
 
         if float(instructionCounts["lea"]) != 0 and (instructionCounts["push"]/float(instructionCounts["lea"])) != 0:
-            output["ln(num push/num lea)"] = math.log(instructionCounts["push"]/float(instructionCounts["lea"]))
+            output["ln(num push/num lea)"] = log(instructionCounts["push"]/float(instructionCounts["lea"]))
         else:
             output["ln(num push/num lea)"] = "infinity"
 
         if float(instructionCounts["test"]) != 0 and (instructionCounts["cmp"]/float(instructionCounts["test"])) != 0:     
-            output["ln(num cmp/num test)"] = math.log(instructionCounts["cmp"]/float(instructionCounts["test"]))
+            output["ln(num cmp/num test)"] = log(instructionCounts["cmp"]/float(instructionCounts["test"]))
         else:
             output["ln(num cmp/num test)"] = "infinity"
 
         if float(instructionCounts["push"]) != 0 and (instructionCounts["mov"]/float(instructionCounts["push"])) != 0:
-            output["ln(num mov/num push)"] = math.log(instructionCounts["mov"]/float(instructionCounts["push"]))
+            output["ln(num mov/num push)"] = log(instructionCounts["mov"]/float(instructionCounts["push"]))
         else:
             output["ln(num mov/num push)"] = "infinity" 
         if float(instructionCounts["lea"]) != 0 and (instructionCounts["jmp"]/float(instructionCounts["lea"])) != 0:
-            output["ln(num jmp/num lea)"] = math.log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
+            output["ln(num jmp/num lea)"] = log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
         else:
             output["ln(num jmp/num lea)"] = "infinity"
 
         if float(instructionCounts["call"]) != 0 and (instructionCounts["indirect_call"]/float(instructionCounts["call"])) != 0:     
-            output["ln(num indirect call/num call)"] = math.log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
+            output["ln(num indirect call/num call)"] = log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
         else:
             output["ln(num indirect call/num call)"] = "infinity"
 
         if float(registerCounts["ecx"]) != 0:    
-            output["ln(num eax/num ecx)"] = math.log(registerCounts["eax"]/float(registerCounts["ecx"]))
+            output["ln(num eax/num ecx)"] = log(registerCounts["eax"]/float(registerCounts["ecx"]))
         else:
             output["ln(num eax/num ecx)"] = "infinity"
         if "esi" in registerCounts.keys() and "edi" in registerCounts.keys():
             if float(registerCounts["edi"]) != 0 and (registerCounts["esi"]/float(registerCounts["edi"])) != 0:
-                output["ln(num esi/num edi)"] = math.log(registerCounts["esi"]/float(registerCounts["edi"]))
+                output["ln(num esi/num edi)"] = log(registerCounts["esi"]/float(registerCounts["edi"]))
             else:
                 output["ln(num esi/num edi)"] = "infinity"
         else:
@@ -130,11 +130,11 @@ class Choice1():
 
         if float(instructionCounts["lea"]) != 0:
             if (instructionCounts["push"]/float(instructionCounts["lea"])) != 0:
-                feature1 = math.log(instructionCounts["push"]/float(instructionCounts["lea"]))
+                feature1 = log(instructionCounts["push"]/float(instructionCounts["lea"]))
             else:
                 feature1 = -1
             if (instructionCounts["jmp"]/float(instructionCounts["lea"])) != 0:
-                feature2 = math.log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
+                feature2 = log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
             else:
                 feature2 = -1
         else:
@@ -142,35 +142,35 @@ class Choice1():
             feature2 = -1
 
         if float(instructionCounts["push"]) != 0 and (instructionCounts["mov"]/float(instructionCounts["push"])) != 0:
-            feature3 = math.log(instructionCounts["mov"]/float(instructionCounts["push"]))
+            feature3 = log(instructionCounts["mov"]/float(instructionCounts["push"]))
         else:
             feature3 = -1
 
         if float(instructionCounts["call"]) != 0 and (instructionCounts["indirect_call"]/float(instructionCounts["call"])) != 0:
-            feature4 = math.log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
+            feature4 = log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
         else:
             feature4 = -1
 
         if float(instructionCounts["test"]) != 0 and (instructionCounts["cmp"]/float(instructionCounts["test"])) != 0:
-            feature5 = math.log(instructionCounts["cmp"]/float(instructionCounts["test"]))
+            feature5 = log(instructionCounts["cmp"]/float(instructionCounts["test"]))
         else:
             feature5 = -1
             
         if float(totalInstructions) != 0:
             if (instructionCounts["reg_used"]/float(totalInstructions)) != 0:
-                feature6 = math.log(instructionCounts["reg_used"]/float(totalInstructions))
+                feature6 = log(instructionCounts["reg_used"]/float(totalInstructions))
             else:
                 feature6 = -1
             if (instructionCounts["push"]/float(totalInstructions)) != 0:
-                feature7 = math.log(instructionCounts["push"]/float(totalInstructions))
+                feature7 = log(instructionCounts["push"]/float(totalInstructions))
             else:
                 feature7 = -1
             if (instructionCounts["call"]/float(totalInstructions)) != 0:
-                feature8 = math.log(instructionCounts["call"]/float(totalInstructions))
+                feature8 = log(instructionCounts["call"]/float(totalInstructions))
             else:
                 feature8 = -1
             if (instructionCounts["indirect_call"]/float(totalInstructions)) != 0:
-                feature9 = math.log(instructionCounts["indirect_call"]/float(totalInstructions))
+                feature9 = log(instructionCounts["indirect_call"]/float(totalInstructions))
             else:
                 feature9 = -1
         else:
@@ -180,13 +180,13 @@ class Choice1():
             feature9 = -1
 
         if float(registerCounts["ecx"]) != 0:
-            feature10 = math.log(registerCounts["eax"]/float(registerCounts["ecx"]))
+            feature10 = log(registerCounts["eax"]/float(registerCounts["ecx"]))
         else:
             feature10 = -1
 
         if "edi" in registerCounts.keys():
             if float(registerCounts["edi"]) != 0:
-                feature11 = math.log(registerCounts["esi"]/float(registerCounts["edi"]))
+                feature11 = log(registerCounts["esi"]/float(registerCounts["edi"]))
             else:
                 feature11 = -1
         else:
@@ -257,56 +257,56 @@ class Choice1():
         output["regs used"] = instructionCounts["reg_used"]
 
         if float(totalInstructions) != 0 :
-            output["ln(num push/length)"] = math.log(instructionCounts["push"]/float(totalInstructions))
-            output["ln(num call/length)"] = math.log(instructionCounts["call"]/float(totalInstructions))
+            output["ln(num push/length)"] = log(instructionCounts["push"]/float(totalInstructions))
+            output["ln(num call/length)"] = log(instructionCounts["call"]/float(totalInstructions))
         else:
             output["ln(num push/length)"] = "infinity"
             output["ln(num call/length)"] = "infinity"
         if float(totalInstructions) != 0:
             if (instructionCounts["indirect_call"]/float(totalInstructions)) != 0 and (instructionCounts["reg_used"]/float(totalInstructions)) != 0:
-                output["ln(num Indirect call/length)"] = math.log(instructionCounts["indirect_call"]/float(totalInstructions))
-                output["ln(num reg used/length)"] = math.log(instructionCounts["reg_used"]/float(totalInstructions))
+                output["ln(num Indirect call/length)"] = log(instructionCounts["indirect_call"]/float(totalInstructions))
+                output["ln(num reg used/length)"] = log(instructionCounts["reg_used"]/float(totalInstructions))
             elif (instructionCounts["indirect_call"]/float(totalInstructions)) == 0:
                 output["ln(num Indirect call/length)"] = "infinity"
-                output["ln(num reg used/length)"] = math.log(instructionCounts["reg_used"]/float(totalInstructions))
+                output["ln(num reg used/length)"] = log(instructionCounts["reg_used"]/float(totalInstructions))
             elif (instructionCounts["reg_used"]/float(totalInstructions)) == 0:
-                output["ln(num Indirect call/length)"] = math.log(instructionCounts["indirect_call"]/float(totalInstructions))
+                output["ln(num Indirect call/length)"] = log(instructionCounts["indirect_call"]/float(totalInstructions))
                 output["ln(num reg used/length)"] = "infinity"
         else:
             output["ln(num Indirect call/length)"] = "infinity"
             output["ln(num reg used/length)"] = "infinity"
 
         if float(instructionCounts["lea"]) != 0 and (instructionCounts["push"]/float(instructionCounts["lea"])) != 0:
-            output["ln(num push/num lea)"] = math.log(instructionCounts["push"]/float(instructionCounts["lea"]))
+            output["ln(num push/num lea)"] = log(instructionCounts["push"]/float(instructionCounts["lea"]))
         else:
             output["ln(num push/num lea)"] = "infinity"
 
         if float(instructionCounts["test"]) != 0 and (instructionCounts["cmp"]/float(instructionCounts["test"])) != 0:     
-            output["ln(num cmp/num test)"] = math.log(instructionCounts["cmp"]/float(instructionCounts["test"]))
+            output["ln(num cmp/num test)"] = log(instructionCounts["cmp"]/float(instructionCounts["test"]))
         else:
             output["ln(num cmp/num test)"] = "infinity"
 
         if float(instructionCounts["push"]) != 0 and (instructionCounts["mov"]/float(instructionCounts["push"])) != 0:
-            output["ln(num mov/num push)"] = math.log(instructionCounts["mov"]/float(instructionCounts["push"]))
+            output["ln(num mov/num push)"] = log(instructionCounts["mov"]/float(instructionCounts["push"]))
         else:
             output["ln(num mov/num push)"] = "infinity" 
         if float(instructionCounts["lea"]) != 0 and (instructionCounts["jmp"]/float(instructionCounts["lea"])) != 0:
-            output["ln(num jmp/num lea)"] = math.log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
+            output["ln(num jmp/num lea)"] = log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
         else:
             output["ln(num jmp/num lea)"] = "infinity"
 
         if float(instructionCounts["call"]) != 0 and (instructionCounts["indirect_call"]/float(instructionCounts["call"])) != 0:     
-            output["ln(num indirect call/num call)"] = math.log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
+            output["ln(num indirect call/num call)"] = log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
         else:
             output["ln(num indirect call/num call)"] = "infinity"
 
         if float(registerCounts["ecx"]) != 0:    
-            output["ln(num eax/num ecx)"] = math.log(registerCounts["eax"]/float(registerCounts["ecx"]))
+            output["ln(num eax/num ecx)"] = log(registerCounts["eax"]/float(registerCounts["ecx"]))
         else:
             output["ln(num eax/num ecx)"] = "infinity"
         if "esi" in registerCounts.keys() and "edi" in registerCounts.keys():
             if float(registerCounts["edi"]) != 0 and (registerCounts["esi"]/float(registerCounts["edi"])) != 0:
-                output["ln(num esi/num edi)"] = math.log(registerCounts["esi"]/float(registerCounts["edi"]))
+                output["ln(num esi/num edi)"] = log(registerCounts["esi"]/float(registerCounts["edi"]))
             else:
                 output["ln(num esi/num edi)"] = "infinity"
         else:
@@ -316,11 +316,11 @@ class Choice1():
 
         if float(instructionCounts["lea"]) != 0:
             if (instructionCounts["push"]/float(instructionCounts["lea"])) != 0:
-                feature1 = math.log(instructionCounts["push"]/float(instructionCounts["lea"]))
+                feature1 = log(instructionCounts["push"]/float(instructionCounts["lea"]))
             else:
                 feature1 = -1
             if (instructionCounts["jmp"]/float(instructionCounts["lea"])) != 0:
-                feature2 = math.log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
+                feature2 = log(instructionCounts["jmp"]/float(instructionCounts["lea"]))
             else:
                 feature2 = -1
         else:
@@ -328,35 +328,35 @@ class Choice1():
             feature2 = -1
 
         if float(instructionCounts["push"]) != 0 and (instructionCounts["mov"]/float(instructionCounts["push"])) != 0:
-            feature3 = math.log(instructionCounts["mov"]/float(instructionCounts["push"]))
+            feature3 = log(instructionCounts["mov"]/float(instructionCounts["push"]))
         else:
             feature3 = -1
 
         if float(instructionCounts["call"]) != 0 and (instructionCounts["indirect_call"]/float(instructionCounts["call"])) != 0:
-            feature4 = math.log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
+            feature4 = log(instructionCounts["indirect_call"]/float(instructionCounts["call"]))
         else:
             feature4 = -1
 
         if float(instructionCounts["test"]) != 0 and (instructionCounts["cmp"]/float(instructionCounts["test"])) != 0:
-            feature5 = math.log(instructionCounts["cmp"]/float(instructionCounts["test"]))
+            feature5 = log(instructionCounts["cmp"]/float(instructionCounts["test"]))
         else:
             feature5 = -1
             
         if float(totalInstructions) != 0:
             if (instructionCounts["reg_used"]/float(totalInstructions)) != 0:
-                feature6 = math.log(instructionCounts["reg_used"]/float(totalInstructions))
+                feature6 = log(instructionCounts["reg_used"]/float(totalInstructions))
             else:
                 feature6 = -1
             if (instructionCounts["push"]/float(totalInstructions)) != 0:
-                feature7 = math.log(instructionCounts["push"]/float(totalInstructions))
+                feature7 = log(instructionCounts["push"]/float(totalInstructions))
             else:
                 feature7 = -1
             if (instructionCounts["call"]/float(totalInstructions)) != 0:
-                feature8 = math.log(instructionCounts["call"]/float(totalInstructions))
+                feature8 = log(instructionCounts["call"]/float(totalInstructions))
             else:
                 feature8 = -1
             if (instructionCounts["indirect_call"]/float(totalInstructions)) != 0:
-                feature9 = math.log(instructionCounts["indirect_call"]/float(totalInstructions))
+                feature9 = log(instructionCounts["indirect_call"]/float(totalInstructions))
             else:
                 feature9 = -1
         else:
@@ -366,13 +366,13 @@ class Choice1():
             feature9 = -1
 
         if float(registerCounts["ecx"]) != 0:
-            feature10 = math.log(registerCounts["eax"]/float(registerCounts["ecx"]))
+            feature10 = log(registerCounts["eax"]/float(registerCounts["ecx"]))
         else:
             feature10 = -1
 
         if "edi" in registerCounts.keys():
             if float(registerCounts["edi"]) != 0:
-                feature11 = math.log(registerCounts["esi"]/float(registerCounts["edi"]))
+                feature11 = log(registerCounts["esi"]/float(registerCounts["edi"]))
             else:
                 feature11 = -1
         else:
